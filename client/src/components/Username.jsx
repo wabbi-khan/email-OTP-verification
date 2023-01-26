9b973c66ea039253e0035cba80d7c2176054dbfb
wabbi-khan <kwahab789@gmail.com>
2023-01-26 18:03:24 +0500

    add formik validation

--- client/src/components/Username.jsx
+++ client/src/components/Username.jsx
@@ -2,9 +2,24 @@ import React from 'react';
 import styles from '../styles/Username.module.css';
 import avatar from '../assets/profile.png';
 import { Link } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { useFormik } from 'formik';
+import { usernameValidate } from '../helper/Validate';
 const Username = () => {
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+    },
+    validate: usernameValidate,
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit: async (values) => {
+      console.log(values);
+    },
+  });
   return (
     <div className="container mx-auto py-2">
+      <Toaster position="top-center" reverseOrder={false}></Toaster>
       <div className="flex justify-center items-center w-full h-full">
         <div className={styles.glass}>
           <div className="title flex flex-col items-center">
@@ -14,13 +29,14 @@ const Username = () => {
             </span>
           </div>
 
-          <form className="py-1">
+          <form className="py-1" onSubmit={formik.handleSubmit}>
             <div className="profile flex justify-center py-4">
               <img src={avatar} className={styles.profile_img} alt="avatar" />
             </div>
 
             <div className="textbox flex flex-col items-center">
               <input
+                {...formik.getFieldProps('username')}
                 className={styles.textbox}
                 type="text"
                 placeholder="Username"
