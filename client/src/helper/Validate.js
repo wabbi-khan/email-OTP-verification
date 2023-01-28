ef5e5f1581797add17b7d38f91d5f47a1afaa5e0
wabbi-khan <kwahab789@gmail.com>
2023-01-28 15:22:20 +0500

    register done

--- client/src/helper/Validate.js
+++ client/src/helper/Validate.js
@@ -15,6 +15,15 @@ function usernameVerify(error = {}, values) {
 
     return error;
 }
+/** validate register form */
+export async function registerValidation(values) {
+    const errors = usernameVerify({}, values);
+    passwordVerify(errors, values);
+    emailVerify(errors, values);
+
+    return errors;
+}
+
 /** validate reset password */
 export async function resetPasswordValidation(values) {
     const errors = passwordVerify({}, values);
@@ -26,22 +35,6 @@ export async function resetPasswordValidation(values) {
     return errors;
 }
 
-/** validate register form */
-// export async function registerValidation(values){
-//     const errors = usernameVerify({}, values);
-//     passwordVerify(errors, values);
-//     emailVerify(errors, values);
-
-//     return errors;
-// }
-
-/** validate profile page */
-// export async function profileValidation(values){
-//     const errors = emailVerify({}, values);
-//     return errors;
-// }
-
-/** ************************************************* */
 /** validate password */
 export async function passwordValidate(values) {
     const errors = passwordVerify({}, values);
@@ -59,6 +52,19 @@ function passwordVerify(error = {}, values) {
         error.password = toast.error('Password must be more than 4 characters...!');
     } else if (!specialChars.test(values.password)) {
         error.password = toast.error('Password must have special character');
+    }
+
+    return error;
+}
+
+/** validate email */
+function emailVerify(error = {}, values) {
+    if (!values.email) {
+        error.email = toast.error('Email Required...!');
+    } else if (values.email.includes(' ')) {
+        error.email = toast.error('Wrong Email...!');
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        error.email = toast.error('Invalid email address...!');
     }
 
     return error;
