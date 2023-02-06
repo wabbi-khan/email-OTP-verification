6746a15315718559b6b297c767d7c2675cf0caa7
wabbi-khan <kwahab789@gmail.com>
2023-02-07 04:03:27 +0500

    create all endpoints of app

---
+++ server/router/route.js
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+const router = Router();
+
+/** import all controllers **/
+import * as controller from '../controllers/appController.js';
+/** POST METHOD **/
+router.route('/register').post(controller.register);
+router.route('registerMail').post(controller.registerMail); //send the email
+router.route('authentication').post((req, res) => res.end()); //authenticate user
+router.route('login').post(controller.login); //login in app
+
+/** GET METHOD **/
+router.route('/user/:username').get(controller.getUser); //user with username
+router.route('/generateOTP').get(controller.generateOTP); // generate random OTP
+router.route('/verifyOTP').get(controller.verifyOTP); // verify generate OTP
+router.route('/createResetSession').get(controller.createResetSession); // reset all variables
+
+/** PUT METHOD **/
+router.route('/updateUser').put(controller.updateUser); //is use to update the user profile
+router.route('/resetPassword').put(controller.register); // use to reset password
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import connect from './database/connection.js';
-
+import router from './router/route.js';
 const app = express();
 
 // ** middlewares **
@@ -15,21 +15,24 @@ const port = 8080;
 
 // HTTP GET REQUEST
 app.get('/', (req, res) => {
-    res.status(201).json('Home Get Request');
+  res.status(201).json('Home Get Request');
 });
+
+/** API ROUTES **/
+app.use('/api', router);
 
 // Start server only when we have valid connection
 
 connect()
-    .then(() => {
-        try {
-            app.listen(port, () => {
-                console.log(`Server connected to http://localhost:${port}`);
-            });
-        } catch (error) {
-            console.log('Cannot connect to the server ');
-        }
-    })
-    .catch((error) => {
-        console.log('Invalid database connection');
-    });
+  .then(() => {
+    try {
+      app.listen(port, () => {
+        console.log(`Server connected to http://localhost:${port}`);
+      });
+    } catch (error) {
+      console.log('Cannot connect to the server ');
+    }
+  })
+  .catch((error) => {
+    console.log('Invalid database connection');
+  });
