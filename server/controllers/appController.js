9532e410203e1cca680d7f3ba71d9a2f5f9217a4
wabbi-khan <kwahab789@gmail.com>
2023-02-07 15:57:08 +0500

    register acc done

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -1,3 +1,5 @@
+import UserModel from '../model/User.model.js';
+import bcrypt from 'bcrypt';
 /** POST: http://localhost:8080/api/register 
  * @param : {
   "username" : "example123",
@@ -12,12 +14,68 @@
 */
 
 export async function register(req, res) {
-  res.json('register route');
+    try {
+        const { username, password, email, profile } = req.body;
+
+        //check the user existing
+        const existUsername = new Promise((resolve, reject) => {
+            UserModel.findOne({ username }, function(err, user) {
+                if (err) reject(new Error(err));
+                if (user) reject({ error: 'Please use unique username' });
+
+                resolve();
+            });
+        });
+
+        //check the email existing
+        const existEmail = new Promise((resolve, reject) => {
+            UserModel.findOne({ email }, function(err, email) {
+                if (err) reject(new Error(err));
+                if (email) reject({ error: 'Please use unique email' });
+
+                resolve();
+            });
+        });
+
+        Promise.all([existUsername, existEmail])
+            .then(() => {
+                if (password) {
+                    bcrypt
+                        .hash(password, 10)
+                        .then((hashedPassword) => {
+                            const user = new UserModel({
+                                username,
+                                password: hashedPassword,
+                                profile: profile || '',
+                                email,
+                            });
+                            //return save result as a response
+                            user
+                                .save()
+                                .then((result) =>
+                                    res.status(201).send({ msg: 'User Register Successfully' })
+                                )
+                                .catch((error) => res.status(500).send(error));
+                        })
+                        .catch((error) => {
+                            return res.status(500).send({
+                                error: 'Enable to hashed password',
+                            });
+                        });
+                }
+            })
+            .catch((error) => {
+                return res.status(500).send({ error });
+            });
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+    // res.json('register route');
 }
 
 /** PUT: http://localhost:8080/api/registerMail */
 export async function registerMail(req, res) {
-  res.json('registerMail route');
+    res.json('registerMail route');
 }
 
 /** POST: http://localhost:8080/api/login 
@@ -28,12 +86,12 @@ export async function registerMail(req, res) {
 */
 
 export async function login(req, res) {
-  res.json('login route');
+    res.json('login route');
 }
 
 /** GET: http://localhost:8080/api/user/example123 */
 export async function getUser(req, res) {
-  res.json('getUSer route');
+    res.json('getUSer route');
 }
 
 /** PUT: http://localhost:8080/api/updateuser 
@@ -47,27 +105,27 @@ body: {
 }
 */
 export async function updateUser(req, res) {
-  res.json('updateUser route');
+    res.json('updateUser route');
 }
 
 /** GET: http://localhost:8080/api/generateOTP */
 export async function generateOTP(req, res) {
-  res.json('generateOTP route');
+    res.json('generateOTP route');
 }
 
 /** GET: http://localhost:8080/api/verifyOTP */
 export async function verifyOTP(req, res) {
-  res.json('verifyOTP route');
+    res.json('verifyOTP route');
 }
 
 // successfully redirect user when OTP is valid
 /** GET: http://localhost:8080/api/createResetSession */
 export async function createResetSession(req, res) {
-  res.json('createResetSession route');
+    res.json('createResetSession route');
 }
 
 // update the password when we have valid session
 /** PUT: http://localhost:8080/api/resetPassword */
 export async function resetPassword(req, res) {
-  res.json('resetPassword route');
+    res.json('resetPassword route');
 }
